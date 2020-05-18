79c79f8b1744f990573243fb093cf38c49a6817b
Angel Lozano <angel.lozano@elecnor.es>
2020-05-18 16:31:09 +0000

    EOEPCA-98 add actual checks for resource access for RPT

--- eoepca_uma/rpt.py
+++ eoepca_uma/rpt.py
@@ -39,10 +39,11 @@ def introspect(rpt: str, pat: str, introspection_endpoint: str, secure: bool = F
     except Exception as e:
         raise Exception("Call to introspection point returned unexpected value: "+str(e))
 
-def valid_token_introspection_data(rpt_info: dict, time_margin: float = 0.0) -> bool:
+def valid_token_introspection_data(rpt_info: dict, resources: List[dict], time_margin: float = 0.0) -> bool:
     """
-    Asserts if a given rpt is valid at this time, using the introspection data from it.
-
+    Asserts if a given rpt is valid at this time, using the introspection data from it, in regards to the resources and scopes requested.
+    Keep in mind this will return False if any scope or resource is denied.
+    
     This validation is done in compliance with RFC7662 'OAuth 2.0 Token Introspection'.
 
     Keep in mind that if the data analyzed is outdated, this function may return the wrong answer, since it doesn't
@@ -59,6 +60,15 @@ def valid_token_introspection_data(rpt_info: dict, time_margin: float = 0.0) -> 
 
     Args:
     - rpt_info = Dict containing the data from an RPT, obtained from a correct 'introspect' call
+    - resources = List of resources to assert permission over.
+                            Format:
+                            [
+                                {
+                                    "resource_id": <str resource id>,
+                                    "resource_scopes": [ <scope 1>, <scope 2>, ...]
+                                },
+                                ...
+                            ]
     - time_margin (Optional) = A time difference to add to the current timestamp, to ensure this token will be valid _at least until_ now + time_margin
    
     Note that there might not be time information in the data, and thus 'time_margin' would do nothing.
@@ -78,14 +88,38 @@ def valid_token_introspection_data(rpt_info: dict, time_margin: float = 0.0) -> 
     
     if "nbf" in rpt_info:
         if rpt_info["nbf"] > (now + time_margin) : return False
-
+    
     # Actual check of validity using the AS' criteria
-    return "active" in rpt_info and rpt_info["active"] == "true"
+    if "active" not in rpt_info or rpt_info["active"] != "true": return False
 
-def is_valid_now(rpt: str, pat: str, introspection_endpoint: str, time_margin: float = 0.0, secure: bool = False) -> dict:
+    # Check against the resources:
+    if "permissions" not in rpt_info: return False
+
+    for requested_resoure in resources:
+        requested_r = requested_resoure["resource_id"]
+        requested_scopes = requested_resoure["resource_scopes"]
+        ok_r = False
+        for allowed_resource in rpt_info["permissions"]:
+            allowed_r = allowed_resource["resource_id"]
+            allowed_scopes = allowed_resource["resource_scopes"]
+            if requested_r == allowed_r:
+                ok_r = True
+                for scope in requested_scopes:
+                    if scope not in allowed_scopes:
+                        return False
+                break
+        
+        if ok_r: return True
+
+    # Fallthrough to false, as a secure default
+    return False
+
+
+def is_valid_now(rpt: str, pat: str, introspection_endpoint: str, resources: List[dict], time_margin: float = 0.0, secure: bool = False) -> bool:
     """
     Uses the introspection from the AS to get updated data about the RPT, then returns True/False
-    if the RPT is valid or not at the moment of the call of this function.
+    if the RPT is valid or not at the moment of the call of this function, in regards to the resources and scopes requested.
+    Keep in mind this will return False if any scope or resource is denied.
 
     This is equal to calling 'introspect' and then 'valid_token_introspection_data'.
 
@@ -96,6 +130,15 @@ def is_valid_now(rpt: str, pat: str, introspection_endpoint: str, time_margin: f
     - rpt = String containing the rpt (token)
     - pat = String containing the pat (token)
     - introspection_endpoint = String containing the url to the AS' introspection endpoint
+    - resources = List of resources to assert permission over.
+                            Format:
+                            [
+                                {
+                                    "resource_id": <str resource id>,
+                                    "resource_scopes": [ <scope 1>, <scope 2>, ...]
+                                },
+                                ...
+                            ]
     - time_margin (Optional) = A time difference to add to the current timestamp, to ensure this token will be valid _at least until_ now + time_margin
     - secure = toggle checking of SSL certificates. Activating this is recommended on production environments
     Note that there might not be time information in the data, and thus 'time_margin' would do nothing.
@@ -109,7 +152,7 @@ def is_valid_now(rpt: str, pat: str, introspection_endpoint: str, time_margin: f
     except:
         return False
     
-    return valid_token_introspection_data(introspection_data, time_margin)
+    return valid_token_introspection_data(introspection_data, resources, time_margin)
 
 
 def request_for_rpt(client_creds_token: str,
