f850becfde7a675e7fd9c6751f8a8effa2a73666
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2021-10-04 14:26:35 +0000

    updated version fix conflict

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = 'eoepca-uma',
-  version = '0.2.3',
+  version = '0.2.4',
   author = 'EOEPCA',
   author_email = 'angel.lozano@deimos-space.com',
   description = 'Python library to interact with UMA protocol',
